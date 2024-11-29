035fa3e7f5836bbfbe6c4accf9df8e9c4cb86010
Everton Diogo <127454600+EDSDIOGO@users.noreply.github.com>
2024-11-29 02:21:04 -0300

    não esta mostrando a quantidade
    até amanhã

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react"
+
 import { Input } from "@/components/input"
-import { View, Button } from "react-native"
+
+import {useProductDatabase} from "@/database/useProductDatabase"
+
+import { View, Button, Alert } from "react-native"
 
 export default function Index() {
-    return ( 
-        <View style={{ flex: 1, justifyContent: "center", 
-        padding: 32, gap: 16 }}> 
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [quantity,setQuantity] = useState("")
+    const [products, setProducts] = useState([])
 
-            <Input placeholder="Nome" />  
-            <Input placeholder="quantidade" />
-            <Button title= "Salvar"/>
+    const productDatabase = useProductDatabase()
+
+    async function create() {
+        try{
+            if(isNaN(Number(quantity))){
+                return Alert.alert("quantidade", "A quantidade Precisa ser um número!")            
+
+            }
+
+            const response = await productDatabase.create({
+                name, 
+                quantity: Number(quantity), 
+            })
+            
+            Alert.alert("Produto cadastradi com o ID: " + response.insertedRowId)
+        }   catch(error){
+            console.log(error)
+        }
+    }
+
+
+
+
+    return ( 
+        <View style={{ flex: 1, justifyContent: "center", padding: 32, gap: 16 }}> 
+
+            <Input placeholder="Nome" onChangeText={setName} value={name}/>  
+            <Input 
+                placeholder="quantidade" 
+                onChangeText={setQuantity} 
+                value={quantity}
+            />
+            <Button title= "Salvar" onPress={create}/>
         
         </View>
     )
